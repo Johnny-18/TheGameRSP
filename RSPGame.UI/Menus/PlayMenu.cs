2da3eca197b633a24ac219114125baa6e105a5c6
vladiolus3 <vladyslav.dovhal@nure.ua>
2021-02-25 18:33:21 +0300

    commit

--- RSPGame.UI/Menus/PlayMenu.cs
+++ RSPGame.UI/Menus/PlayMenu.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using RSPGame.Models;
 using RSPGame.UI.PlayRequests;
--- RSPGame.UI/PlayRequests/GameRequests.cs
+++ RSPGame.UI/PlayRequests/GameRequests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace RSPGame.UI.PlayRequests
