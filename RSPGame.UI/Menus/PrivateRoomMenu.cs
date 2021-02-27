a34c5b7a2391987f0c53e6b801e7463d22d20d0b
vladiolus3 <vladyslav.dovhal@nure.ua>
2021-02-27 19:52:47 +0300

    oop menu

--- RSPGame.UI/Menus/PrivateRoomMenu.cs
+++ RSPGame.UI/Menus/PrivateRoomMenu.cs
@@ -42,59 +42,66 @@ namespace RSPGame.UI.Menus
                 switch (num)
                 {
                     case 1:
-                        var json = await RoomRequests.PostAsync(_client, _currentSession.GamerInfo, "create");
-                        if (json == null)
-                        {
-                            Console.WriteLine("\nERROR:\tCheck your internet connection\n\n");
-                            break;
-                        }
-                        var id1 = JsonConvert.DeserializeObject<int>(json);
-
-                        Console.WriteLine($"\nRoom with id {id1} has been created!");
-                        Console.WriteLine("\nWaiting for opponent\n\n");
-
-                        var result1 = (await GameRequests.GetGame(_client, id1))?.ToArray();
-                        if (result1 == null) break;
-
-                        var opponent1 = result1
-                            .FirstOrDefault(x => !x.Equals(_currentSession.GamerInfo.UserName));
-
-                        new GameLogic().StartGame(_client, _currentSession.GamerInfo.UserName, opponent1, id1);
-
-                        await _client.DeleteAsync($"api/rooms/stop/{id1}");
+                        CreateRoomAction();
                         break;
-
                     case 2:
-                        Console.Write("Enter the id of the desired room: ");
-
-                        if (!int.TryParse(Console.ReadLine(), out var id2))
-                        {
-                            Console.WriteLine("\nERROR:\tThe only numbers can be entered. Try again\n\n");
-                            break;
-                        }
-                        else if (id2 < 1 || id2 > 1000)
-                        {
-                            Console.WriteLine("\nERROR:\tIncorrect number. Try again\n\n");
-                            break;
-                        }
-
-                        if (RoomRequests.JoinAsync(_client, _currentSession.GamerInfo, id2) == null)
-                            break;
-
-                        var result2 = (await GameRequests.GetGame(_client, id2))?.ToArray();
-                        if (result2 == null) break;
-
-                        var opponent2 = result2
-                            .FirstOrDefault(x => !x.Equals(_currentSession.GamerInfo.UserName));
-
-                        new GameLogic().StartGame(_client, _currentSession.GamerInfo.UserName, opponent2, id2);
-
+                        JoinRoomAction();
                         break;
-
                     case 3:
                         return;
                 }
             }
+        }
+
+        public async void CreateRoomAction()
+        {
+            var json = await RoomRequests.PostAsync(_client, _currentSession.GamerInfo, "create");
+            if (json == null)
+            {
+                Console.WriteLine("\nERROR:\tCheck your internet connection\n\n");
+                return;
+            }
+            var id = JsonConvert.DeserializeObject<int>(json);
+
+            Console.WriteLine($"\nRoom with id {id} has been created!");
+            Console.WriteLine("\nWaiting for opponent\n\n");
+
+            var result = (await GameRequests.GetGame(_client, id))?.ToArray();
+            if (result == null) return;
+
+            var opponent1 = result
+                .FirstOrDefault(x => !x.Equals(_currentSession.GamerInfo.UserName));
+
+            new GameLogic().StartGame(_client, _currentSession.GamerInfo.UserName, opponent1, id);
+
+            await _client.DeleteAsync($"api/rooms/stop/{id}");
+        }
+
+        public async void JoinRoomAction()
+        {
+            Console.Write("Enter the id of the desired room: ");
+
+            if (!int.TryParse(Console.ReadLine(), out var id))
+            {
+                Console.WriteLine("\nERROR:\tThe only numbers can be entered. Try again\n\n");
+                return;
+            }
+            else if (id < 1 || id > 1000)
+            {
+                Console.WriteLine("\nERROR:\tIncorrect number. Try again\n\n");
+                return;
+            }
+
+            if (RoomRequests.JoinAsync(_client, _currentSession.GamerInfo, id) == null)
+                return;
+
+            var result = (await GameRequests.GetGame(_client, id))?.ToArray();
+            if (result == null) return;
+
+            var opponent2 = result
+                .FirstOrDefault(x => !x.Equals(_currentSession.GamerInfo.UserName));
+
+            new GameLogic().StartGame(_client, _currentSession.GamerInfo.UserName, opponent2, id);
         }
     }
 }
